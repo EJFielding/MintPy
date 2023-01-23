f279d47087c709d391131c64c9cd23c7c3d7aad6
Eric Fielding <EJFielding@users.noreply.github.com>
2023-01-22 19:53:42 -0800

    Create convert_geom_gmt.py
    first version convert_geom_gmt

---
+++ src/mintpy/convert_geom_gmt.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.io import netcdf
+
+# routines from MintPy
+from mintpy.utils import readfile, plot as pp
+
+# routines copied from mintpy/save_gmt.py 
+def write_gmt_simple(lons, lats, z, fname, title='default', name='z', scale=1.0, offset=0, units='meters'):
+    """Writes a simple GMT grd file with one array.
+    This is based on the gdal2grd.py script found at:
+        http://http://www.vso.cape.com/~nhv/files/python/gdal/gdal2grd.py
+
+    Parameters: lons : 1D Array of lon values
+                lats : 1D Array of lat values
+                z : 2D slice to be saved
+                fname : Output file name
+    Kwargs:     title : Title for the grd file
+                name : Name of the field in the grd file
+                scale : Scale value in the grd file
+                offset : Offset value in the grd file
+    Returns:    fname
+    """
+
+    fid = netcdf.netcdf_file(fname, 'w')
+
+    # Create a dimension variable
+    fid.createDimension('side', 2)
+    fid.createDimension('xysize', np.prod(z.shape))
+
+    # Range variables
+    fid.createVariable('x_range', 'd', ('side',))
+    fid.variables['x_range'].units = 'degrees'
+
+    fid.createVariable('y_range', 'd', ('side',))
+    fid.variables['y_range'].units = 'degrees'
+
+    fid.createVariable('z_range', 'd', ('side',))
+    fid.variables['z_range'].units = units
+
+    # Spacing
+    fid.createVariable('spacing', 'd', ('side',))
+    fid.createVariable('dimension', 'i4', ('side',))
+
+    fid.createVariable('z', 'f', ('xysize',))
+    fid.variables['z'].long_name = name
+    fid.variables['z'].scale_factor = scale
+    fid.variables['z'].add_offset = offset
+    fid.variables['z'].node_offset = 0
+
+    fid.title = title
+    fid.source = 'MintPy'
+
+    # Filling in the actual data
+    fid.variables['x_range'][0] = lons[0]
+    fid.variables['x_range'][1] = lons[-1]
+    fid.variables['spacing'][0] = lons[1]-lons[0]
+
+    fid.variables['y_range'][0] = lats[0]
+    fid.variables['y_range'][1] = lats[-1]
+    fid.variables['spacing'][1] = lats[1]-lats[0]
+
+    # Range
+    fid.variables['z_range'][0] = np.nanmin(z)
+    fid.variables['z_range'][1] = np.nanmax(z)
+
+    fid.variables['dimension'][:] = z.shape[::-1]
+    fid.variables['z'][:] = np.flipud(z).flatten()
+    fid.close()
+    return fname
+    ############################################################
+    # Program is part of GIAnT v1.0                            #
+    # Copyright 2012, by the California Institute of Technology#
+    # Contact: earthdef@gps.caltech.edu                        #
+    ############################################################
+
+
+def get_geo_lat_lon(atr):
+    X_FIRST = float(atr['X_FIRST'])
+    Y_FIRST = float(atr['Y_FIRST'])
+    X_STEP = float(atr['X_STEP'])
+    Y_STEP = float(atr['Y_STEP'])
+    W = int(atr['WIDTH'])
+    L = int(atr['LENGTH'])
+    Y_END = Y_FIRST + L*Y_STEP
+    X_END = X_FIRST + W*X_STEP
+
+    X = np.linspace(X_FIRST, X_END, W)
+    Y = np.linspace(Y_FIRST, Y_END, L)
+    #XI,YI = np.meshgrid(X,Y)
+
+    return Y, X
+
+
+def write_grd_file(data, atr, fname_out=None):
+    """Write GMT .grd file for input data matrix, using giant._gmt module.
+    Inputs:
+        data - 2D np.array in int/float, data matrix to write
+        atr  - dict, attributes of input data matrix
+        fname_out - string, output file name
+    Output:
+        fname_out - string, output file name
+    """
+    # Get 1D array of lats and lons
+    lats, lons = get_geo_lat_lon(atr)
+
+    # writing
+    print('writing >>> '+fname_out)
+    write_gmt_simple(lons, np.flipud(lats), np.flipud(data), fname_out,
+                     title='default', name=atr['FILE_TYPE'],
+                     scale=1.0, offset=0, units=atr['UNIT'])
+    return fname_out
+
+####################################################################################
+def convert_geom_gmt(inps):
+    
+    if not inps.file:
+        inps.file = 'geo/geo_geometryRadar.h5'  
+
+    # Read incidence angle data from original MintPy to get all the attributes
+    dset = 'incidenceAngle'
+    dataI, atr = readfile.read(inps.file, datasetName=dset) 
+
+    dset = 'azimuthAngle'
+    dataA, atr1 = readfile.read(inps.file, datasetName=dset) 
+
+    # calculate components
+    U=np.cos(np.deg2rad(dataI))
+    N=np.sin(np.deg2rad(dataI)) * np.sin(np.deg2rad(dataA+90))
+    E=np.sin(np.deg2rad(dataI)) * np.cos(np.deg2rad(dataA+90))
+
+    outEname = 'LOSeast.grd'
+    outfile = write_grd_file(E, atr, outEname)
+
+    outNname = 'LOSnorth.grd'
+    outfile = write_grd_file(N, atr, outNname)
+
+    outUname = 'LOSup.grd'
+    outfile = write_grd_file(U, atr, outUname)
+
+
+    print('Done.')
+    return
